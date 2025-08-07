4d7a924c6e3a0da1644e923dda9195156291f894
Thomas <59429963+tfhammond@users.noreply.github.com>
2025-08-06 20:56:58 -0700

    recompiled

---
+++ portfolio_website/next.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  output: "export",
+
+  basePath: "/tfhammond.github.io",
+
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig
